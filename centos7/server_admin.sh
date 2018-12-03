ad756c1dc2d49afd269f9a6fd33da545493516c0
Trinh Thanh Phuong <iphuongtt@gmail.com>
2018-12-03 15:32:09 +0700

    install nginx

--- centos7/server_admin.sh
+++ centos7/server_admin.sh
@@ -7,7 +7,7 @@ if [ $(id -u) != "0" ]; then
 fi
 
 prompt="Chose your action:"
-options=("Create database", "Install Mariadb 10.1") # End Options
+options=("Create database", "Install Mariadb 10.1", "Install Nginx") # End Options
 
 printf "=========================================================================\n"
 printf "                             Server Admin Menu\n"
@@ -19,6 +19,7 @@ select opt in "${options[@]}" "Exit"; do
     case "$REPLY" in
 	    1 ) /etc/server_admin/menu/create_database;;
 		2 ) /etc/server_admin/menu/install_mariadb;;
+		3 ) /etc/server_admin/menu/install_nginx;;
 	    # End Menu
 
 	    $(( ${#options[@]}+1 )) ) printf "\nBye\n\n"; break;;
