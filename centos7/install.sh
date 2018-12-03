35f354ffe9c8e3d956514b6def41a0ed6bdae655
Trinh Thanh Phuong <iphuongtt@gmail.com>
2018-12-03 14:27:45 +0700

    need root permission

--- centos7/install.sh
+++ centos7/install.sh
@@ -1,5 +1,8 @@
 #!bin/sh
-
+if [ $(id -u) != "0" ]; then
+    printf "You need to be root to perform this command. Run \"sudo su\" to become root!\n"
+    exit
+fi
 mkdir -p /etc/server_admin/menu/
 cp server_admin.sh /bin/server-admin && chmod +x /bin/server-admin
 cp menu/* /etc/server_admin/menu/
