7738b9cb966214f6f14f658a466aec873ed8ad94
iphuongtt <phuong.h3t@gmail.com>
2018-12-21 05:14:41 +0700

    Update install.sh

--- centos7/install.sh
+++ centos7/install.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 clear
 
-. ~/server_admin_script/centos7/menu/color.sh
-. ~/server_admin_script/centos7/menu/global_var
-. ~/server_admin_script/centos7/menu/function.sh
+. /home/root/server_admin_script/centos7/menu/color.sh
+. /home/root/server_admin_script/centos7/menu/global_var
+. /home/root/server_admin_script/centos7/menu/function.sh
 
 if [ $(id -u) != "0" ]; then
     printf "You need to be root to perform this command. Run \"sudo su\" to become root!\n"
@@ -11,8 +11,8 @@ if [ $(id -u) != "0" ]; then
 fi
 
 mkdir -p /etc/server_admin/menu/
-cp ~/server_admin_script/centos7/server_admin.sh /bin/server-admin && chmod +x /bin/server-admin
-cp ~/server_admin_script/centos7/menu/* /etc/server_admin/menu/
+cp /home/root/server_admin_script/centos7/server_admin.sh /bin/server-admin && chmod +x /bin/server-admin
+cp /home/root/server_admin_script/centos7/menu/* /etc/server_admin/menu/
 chmod +x /etc/server_admin/menu/*
 
 update_system
