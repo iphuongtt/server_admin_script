fdcdc739131eab930925d1ddc99e676f30f63697
Trinh Thanh Phuong <iphuongtt@gmail.com>
2018-12-21 09:15:11 +0700

    fix error

--- centos7/install.sh
+++ centos7/install.sh
@@ -1,9 +1,14 @@
 #!bin/bash
 clear
 
-. /home/root/server_admin_script/centos7/menu/color.sh
-. /home/root/server_admin_script/centos7/menu/global_var
-. /home/root/server_admin_script/centos7/menu/function.sh
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+. $SCRIPTPATH/menu/color.sh
+. $SCRIPTPATH/menu/global_var
+. $SCRIPTPATH/menu/function.sh
 
 if [ $(id -u) != "0" ]; then
     printf "You need to be root to perform this command. Run \"sudo su\" to become root!\n"
@@ -11,8 +16,8 @@ if [ $(id -u) != "0" ]; then
 fi
 
 mkdir -p /etc/server_admin/menu/
-cp /home/root/server_admin_script/centos7/server_admin.sh /bin/server-admin && chmod +x /bin/server-admin
-cp /home/root/server_admin_script/centos7/menu/* /etc/server_admin/menu/
+cp $SCRIPTPATH/server_admin.sh /bin/server-admin && chmod +x /bin/server-admin
+cp $SCRIPTPATH/menu/* /etc/server_admin/menu/
 chmod +x /etc/server_admin/menu/*
 
 update_system
