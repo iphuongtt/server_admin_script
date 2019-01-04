62ce7b92d428be55a9e4d56edb6bfe1cfb4cdc59
Trinh Thanh Phuong <iphuongtt@gmail.com>
2019-01-04 14:23:16 +0700

    edit server-admin menu

--- centos7/server_admin.sh
+++ centos7/server_admin.sh
@@ -1,8 +1,71 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Renders a text based list of options that can be selected by the
+# user using up, down and enter keys and returns the chosen option.
+#
+#   Arguments   : list of options, maximum of 256
+#                 "opt1" "opt2" ...
+#   Return value: selected index (0 for opt1, 1 for opt2 ...)
+. /etc/server_admin/menu/color.sh
+
+function select_option {
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        { read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi; }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - $#))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for opt; do
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
 
 clear
-
-. /etc/server_admin/menu/color.sh
 
 installNginx="${BCyan}3.  INSTALL NGINX${Color_Off}"
 
@@ -35,11 +98,13 @@ show_menus() {
 	echo -e "${BRed}17.  EXIT${Color_Off}"
 }
 
-show_root_menus() {
-	clear
+show_title() {
 	echo -e "${BGreen}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${Color_Off}"	
 	echo -e "${BYellow} S E R V E R - A D M I N - M E N U ${Color_Off}"
 	echo -e "${BGreen}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${Color_Off}"	
+}
+show_root_menus() {
+	clear
 	echo -e "${BCyan}1.  DEVELOPMENTS${Color_Off}"
 	echo -e "${BCyan}2.  NETWORKS${Color_Off}"
 	echo -e "${BCyan}3.  SYSTEMS${Color_Off}"
@@ -89,10 +154,23 @@ read_options(){
 	esac
 }
 
+action() {
+	case $1 in
+		1) /etc/server_admin/menu/developments/menu; break ;;
+		2) /etc/server_admin/menu/networks/menu; break ;;
+		3) /etc/server_admin/menu/systems/menu; break ;;
+		4) /etc/server_admin/menu/webservers/menu; break ;;
+		5) /etc/server_admin/menu/utilities/menu; break ;;
+		6) /etc/server_admin/menu/selt_update_tool; break ;;
+		7) exit 0;;
+		*) echo -e "${RED}Error...${Color_Off}"
+	esac
+}
+clear
 trap 'return_menu' SIGINT SIGQUIT SIGTSTP
 
-while true
-do
-	show_root_menus
-	read_root_menu_options
-done
+show_title
+options=("DEVELOPMENTS" "NETWORKS" "SYSTEMS" "WEB SERVERS" "UTILITIES" "SELF UPDATE TOOL" "EXIT")
+select_option "${options[@]}"
+choice=$?
+action choice
