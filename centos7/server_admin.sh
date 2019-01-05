b2764ecff5062d8774612d10aabdf7b6cb3f8480
Trinh Thanh Phuong <iphuongtt@gmail.com>
2019-01-05 08:20:33 +0700

    fix error menu

--- centos7/server_admin.sh
+++ centos7/server_admin.sh
@@ -87,12 +87,12 @@ show_menu() {
 
 read_options() {
 	case $1 in
-		1) /etc/server_admin/menu/developments/menu; break ;;
-		2) /etc/server_admin/menu/networks/menu; break ;;
-		3) /etc/server_admin/menu/systems/menu; break ;;
-		4) /etc/server_admin/menu/webservers/menu; break ;;
-		5) /etc/server_admin/menu/utilities/menu; break ;;
-		6) /etc/server_admin/menu/selt_update_tool; break ;;
+		1) /etc/server_admin/menu/developments/menu; exit ;;
+		2) /etc/server_admin/menu/networks/menu; exit ;;
+		3) /etc/server_admin/menu/systems/menu; exit ;;
+		4) /etc/server_admin/menu/webservers/menu; exit ;;
+		5) /etc/server_admin/menu/utilities/menu; exit ;;
+		6) /etc/server_admin/menu/selt_update_tool; exit ;;
 		7) clear; exit 0;;
 		*) clear && show_root_new_menu
 	esac
